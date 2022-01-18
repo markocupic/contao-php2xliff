5926495ad384c0fd3e258c9dbb66cf857b7394d6
Marko Cupic <m.cupic@gmx.ch>
2022-01-18 13:38:27 +0100

    Fix wrong ecs path

--- .ecs/config/set/header_comment_fixer.php
+++ .ecs/config/set/header_comment_fixer.php
@@ -12,7 +12,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services
         ->set(HeaderCommentFixer::class)
         ->call('configure', [[
-            'header' => "This file is part of Contao PHP language file to XLIFF.\n\n(c) Marko Cupic 2021 <m.cupic@gmx.ch>\n@license GPL-3.0-or-later\nFor the full copyright and license information,\nplease view the LICENSE file that was distributed with this source code.\n@link https://github.com/markocupic/contao-php2xliff",
+            'header' => "This file is part of Contao Php2Xliff.\n\n(c) Marko Cupic ".date('Y')." <m.cupic@gmx.ch>\n@license GPL-3.0-or-later\nFor the full copyright and license information,\nplease view the LICENSE file that was distributed with this source code.\n@link https://github.com/markocupic/contao-php2xliff",
         ]])
     ;
 };
